d8e8d4990854d2d9516e2591b2ff70f52b1cd173
Vtorygin Valerii <vtorygin.vv@gmail.com>
2021-08-04 17:13:51 +0300

    Rename modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,14 @@
-#[path = "1_two_sum/two_sum.rs"]
+#[path = "0001_two_sum/two_sum.rs"]
 pub mod two_sum;
 
-#[path = "7_reverse_integer/reverse_integer.rs"]
+#[path = "0007_reverse_integer/reverse_integer.rs"]
 pub mod reverse_integer;
 
-#[path = "9_palindrome_number/palindrome_number.rs"]
+#[path = "0009_palindrome_number/palindrome_number.rs"]
 pub mod palindrome_number;
 
-#[path = "13_roman_to_integer/roman_to_integer.rs"]
+#[path = "0013_roman_to_integer/roman_to_integer.rs"]
 pub mod roman_to_int;
 
-#[path = "14_longest_common_prefix/longest_common_prefix.rs"]
+#[path = "0014_longest_common_prefix/longest_common_prefix.rs"]
 pub mod longest_common_prefix;
